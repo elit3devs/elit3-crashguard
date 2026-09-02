1f942898806cb6318391d2d4e9dae63e20c66a41
Elit3 <bob691388@gmail.com>
2026-09-02 17:29:11 -0400

    .

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 name 'elit3-crashguard'
 author 'Elit3'
 description 'Elit3 Crash Guard - blocks known crash methods used by cheat menus.'
-version '1.1.0'
+version '1.2.0'
 
 server_scripts {
     'server/main.lua'
